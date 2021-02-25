77bb075c884349743c72019c45f348eac5904fd1
Justyna <kowalkowska.just@gmail.com>
2021-02-25 22:51:23 +0100

    Initial commit.

---
+++ NewsApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp-MVVM
+//
+//  Created by Justyna Kowalkowska on 25/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
